6e540e1297506c866ef128a2dd090a51906a865f
Brayan Sigala <brayansigala21@gmail.com>
2023-11-18 23:39:09 -0600

    change execute_js

--- execute_js_code_scripts/estadosDeCuenta_bot_main_2.js
+++ execute_js_code_scripts/estadosDeCuenta_bot_main_2.js
@@ -1,3 +1,2 @@
-const index = excel_content.Sheet2.findIndex(item => item.CARGOS === "[SALDO INICIAL DE 0.00]");
-
-excel_content = excel_content.Sheet2.slice(0, index + 1);
+const index = excel_content.Sheet2.findIndex(row => row.CARGOS && typeof row.CARGOS === 'string' && row.CARGOS.includes("[SALDO INICIAL DE"))
+excel_content = index !== -1 ? excel_content.Sheet2.slice(0, index + 1) : excel_content.Sheet2
